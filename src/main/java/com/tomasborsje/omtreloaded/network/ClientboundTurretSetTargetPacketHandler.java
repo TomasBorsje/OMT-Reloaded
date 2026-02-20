9350cf7386b9bc878f6c8f15cfc9864afd550e37
TomasBorsje <t.borsje2@gmail.com>
2026-02-21 11:59:58 +1300

    fix: Check level existence when receiving packets

--- src/main/java/com/tomasborsje/omtreloaded/network/ClientboundTurretSetTargetPacketHandler.java
+++ src/main/java/com/tomasborsje/omtreloaded/network/ClientboundTurretSetTargetPacketHandler.java
@@ -2,8 +2,14 @@ package com.tomasborsje.omtreloaded.network;
 
 import com.tomasborsje.omtreloaded.core.AbstractTurretBlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
+
+import java.io.IOException;
 
 /**
  * When our client receives a ClientboundTurretSetTargetPacket, set the turret at the given location's target to the
@@ -11,9 +17,17 @@ import net.neoforged.neoforge.network.handling.IPayloadContext;
  */
 public class ClientboundTurretSetTargetPacketHandler {
     public static void handleDataOnMain(ClientboundTurretSetTargetPacket packet, IPayloadContext context) {
-        BlockEntity be = context.player().level().getBlockEntity(new BlockPos(packet.blockX(), packet.blockY(), packet.blockZ()));
-        if(be instanceof AbstractTurretBlockEntity turretBlockEntity) {
-            turretBlockEntity.setTargetByEntityId(packet.entityId());
+        try (var level = context.player().level()) {
+            final BlockPos blockPos = new BlockPos(packet.blockX(), packet.blockY(), packet.blockZ());
+            final ChunkPos chunkPos = new ChunkPos(blockPos);
+            if(!level.hasChunk(chunkPos.x, chunkPos.z)) { return; }
+
+            BlockEntity be = level.getBlockEntity(blockPos);
+            if(be instanceof AbstractTurretBlockEntity turretBlockEntity) {
+                turretBlockEntity.setTargetByEntityId(packet.entityId());
+            }
+        } catch (IOException e) {
+            // Don't do anything
         }
     }
 }
--- src/main/java/com/tomasborsje/omtreloaded/network/ServerboundRequestTurretTargetPacketHandler.java
+++ src/main/java/com/tomasborsje/omtreloaded/network/ServerboundRequestTurretTargetPacketHandler.java
@@ -5,9 +5,12 @@ import com.tomasborsje.omtreloaded.core.AbstractTurretBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
+
+import java.io.IOException;
 
 /**
  * When the client requests a target, send them a ClientboundTurretSetTargetPacket with the requested turret's current
@@ -15,16 +18,24 @@ import net.neoforged.neoforge.network.handling.IPayloadContext;
  */
 public class ServerboundRequestTurretTargetPacketHandler {
     public static void handleDataOnMain(ServerboundRequestTurretTargetPacket packet, IPayloadContext context) {
-        BlockEntity be = context.player().level().getBlockEntity(new BlockPos(packet.blockX(), packet.blockY(), packet.blockZ()));
-        if(be instanceof AbstractTurretBlockEntity turretBlockEntity) {
-            // Send the client a turret set target packet to sync
-            final BlockPos pos = turretBlockEntity.getBlockPos();
-            final Entity turretTarget = turretBlockEntity.getTargetEntity();
-            final ClientboundTurretSetTargetPacket turretTargetPacket;
-            if (turretTarget != null) {
-                turretTargetPacket = new ClientboundTurretSetTargetPacket(pos.getX(), pos.getY(), pos.getZ(), turretBlockEntity.getTargetEntity().getId());
-                PacketDistributor.sendToPlayer((ServerPlayer) context.player(), turretTargetPacket);
+        try (var level = context.player().level()) {
+            final BlockPos blockPos = new BlockPos(packet.blockX(), packet.blockY(), packet.blockZ());
+            final ChunkPos chunkPos = new ChunkPos(blockPos);
+            if(!level.hasChunk(chunkPos.x, chunkPos.z)) { return; }
+
+            BlockEntity be = level.getBlockEntity(blockPos);
+            if(be instanceof AbstractTurretBlockEntity turretBlockEntity) {
+                // Send the client a turret set target packet to sync
+                final BlockPos pos = turretBlockEntity.getBlockPos();
+                final Entity turretTarget = turretBlockEntity.getTargetEntity();
+                final ClientboundTurretSetTargetPacket turretTargetPacket;
+                if (turretTarget != null) {
+                    turretTargetPacket = new ClientboundTurretSetTargetPacket(pos.getX(), pos.getY(), pos.getZ(), turretBlockEntity.getTargetEntity().getId());
+                    PacketDistributor.sendToPlayer((ServerPlayer) context.player(), turretTargetPacket);
+                }
             }
+        } catch (IOException e) {
+            // Don't do anything
         }
     }
 }
